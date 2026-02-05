bc986b452e0ae6c310cc14e58d6ff44f96f3a9db
Joel Jacob <joeljacobillikkaparmbil@gmail.com>
2026-02-05 09:10:36 +0530

    completed UI

--- lib/features/domain/repositories/track_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/track.dart';
-
-abstract class TrackRepository {
-  Future<int> createTrack(Track track);
-  Future<List<Track>> getAllTracks();
-  Future<void> updateTrackName(int trackId, String newName);
-  Future<void> deleteTrack(int trackId);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:progress/app.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'features/data/repositories/track_repository_impl.dart';
-//import 'features/data/repositories/progress_repository_impl.dart';
-import 'features/domain/usecases/create_track.dart';
-import 'features/domain/usecases/delete_track.dart';
-import 'features/domain/usecases/get_tracks.dart';
-import 'features/presentation/bloc/track_bloc.dart';
-import 'features/presentation/bloc/track_event.dart';
 
 void main() {
-  final trackRepository = TrackRepositoryImpl();
-  //final progressRepository = ProgressRepositoryImpl();
-
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => TrackBloc(
-            getTracks: GetTracks(trackRepository),
-            createTrack: CreateTrack(trackRepository),
-            deleteTrack: DeleteTrack(trackRepository),
-          )..add(LoadTracks()),
-        ),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
